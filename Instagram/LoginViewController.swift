000a59fcdd38368b65a28efa3988b00fc9c3b843
Masashi Motohashi <masashi6225@motohashisusumushi-no-MacBook-Pro.local>
2017-05-27 19:49:36 +0900

    loginviewcontroller

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -1,25 +1,9 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Instagram
 //
 //  Created by Masashi Motohashi on 2017/05/27.
 //  Copyright © 2017 masashi.motohashi. All rights reserved.
 //
 
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
-
+import Foundation
