8e4e7401f5d6135618cc1cd31cc9554e25d1b0ae
Masashi Motohashi <masashi6225@motohashisusumushi-no-MacBook-Pro.local>
2017-06-03 18:51:20 +0900

    edit on sat

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -1,14 +1,7 @@
-//
-//  LoginViewController.swift
-//  Instagram
-//
-//  Created by Masashi Motohashi on 2017/05/27.
-//  Copyright © 2017 masashi.motohashi. All rights reserved.
-//
-
 import UIKit
 import Firebase
 import FirebaseAuth
+import SVProgressHUD
 
 class LoginViewController: UIViewController {
     
@@ -18,6 +11,33 @@ class LoginViewController: UIViewController {
     
     // ログインボタンをタップしたときに呼ばれるメソッド
     @IBAction func handleLoginButton(_ sender: Any) {
+        if let address = mailAddressTextField.text, let password = passwordTextField.text {
+            
+            // アドレスとパスワード名のいずれかでも入力されていない時は何もしない
+            if address.characters.isEmpty || password.characters.isEmpty {
+                SVProgressHUD.showError(withStatus: "必要項目を入力して下さい")
+                return
+            }
+            
+            // HUDで処理中を表示
+            SVProgressHUD.show()
+            
+            FIRAuth.auth()?.signIn(withEmail: address, password: password) { user, error in
+                if let error = error {
+                    print("DEBUG_PRINT: " + error.localizedDescription)
+                    SVProgressHUD.showError(withStatus: "サインインに失敗しました。")
+                    return
+                } else {
+                    print("DEBUG_PRINT: ログインに成功しました。")
+                    
+                    // HUDを消す
+                    SVProgressHUD.dismiss()
+                    
+                    // 画面を閉じてViewControllerに戻る
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
     }
     
     // アカウント作成ボタンをタップしたときに呼ばれるメソッド
@@ -27,15 +47,19 @@ class LoginViewController: UIViewController {
             // アドレスとパスワードと表示名のいずれかでも入力されていない時は何もしない
             if address.characters.isEmpty || password.characters.isEmpty || displayName.characters.isEmpty {
                 print("DEBUG_PRINT: 何かが空文字です。")
+                SVProgressHUD.showError(withStatus: "必要項目を入力して下さい")
                 return
             }
             
+            // HUDで処理中を表示
+            SVProgressHUD.show()
             
             // アドレスとパスワードでユーザー作成。ユーザー作成に成功すると、自動的にログインする
             FIRAuth.auth()?.createUser(withEmail: address, password: password) { user, error in
                 if let error = error {
                     // エラーがあったら原因をprintして、returnすることで以降の処理を実行せずに処理を終了する
                     print("DEBUG_PRINT: " + error.localizedDescription)
+                    SVProgressHUD.showError(withStatus: "ユーザー作成に失敗しました。")
                     return
                 }
                 print("DEBUG_PRINT: ユーザー作成に成功しました。")
@@ -47,10 +71,13 @@ class LoginViewController: UIViewController {
                     changeRequest.displayName = displayName
                     changeRequest.commitChanges { error in
                         if let error = error {
+                            SVProgressHUD.showError(withStatus: "ユーザー作成時にエラーが発生しました。")
                             print("DEBUG_PRINT: " + error.localizedDescription)
                         }
                         print("DEBUG_PRINT: [displayName = \(user.displayName!)]の設定に成功しました。")
                         
+                        // HUDを消す
+                        SVProgressHUD.dismiss()
                         
                         // 画面を閉じてViewControllerに戻る
                         self.dismiss(animated: true, completion: nil)
@@ -61,27 +88,12 @@ class LoginViewController: UIViewController {
             }
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-    */
-
 }
